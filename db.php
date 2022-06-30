25b77af1d24c9b8b0e8e74e1436f5722221ac684
joseph-schiro <joseph.schiro@protonmail.com>
2022-06-29 21:31:39 -0400

    Fixed DB Connection Issues

--- db.php
+++ db.php
@@ -1,7 +1,7 @@
 <?php
     // Enter your host name, database username, password, and database name.
     // If you have not set database password on localhost then set empty.
-    $con = mysqli_connect("72.230.138.40","root","My$qL_R00T_!?","LoginSystem");
+    $con = mysqli_connect("localhost", "web", "password");
     // Check connection
     if (mysqli_connect_errno()){
         echo "Failed to connect to MySQL: " . mysqli_connect_error();
